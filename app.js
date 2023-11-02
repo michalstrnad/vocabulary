aba65123daa9b4e9388fe5d217a2f1b1f7bd7e58
Michal Strnad <m.strnad007@gmail.com>
2023-11-02 21:25:15 +0100

    nevim

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + "public"));
+app.use(express.static(__dirname + "/public"));
 
 const mongoose = require("mongoose");
 
